ddd477c20588b1213a2059d9604bc25d02f2aec8
Mathias <mathais234@gmail.com>
2020-05-05 21:00:25 +0200

    Error melding hvis feil skjer under registrering

--- Klient/src/com/obj2000/Main.java
+++ Klient/src/com/obj2000/Main.java
@@ -2,6 +2,7 @@ package com.obj2000;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -24,8 +25,12 @@ public class Main extends Application {
         scene1 = new Scene(registeringsPane, 400, 500);
 
         registeringsPane.registrer.setOnAction(e -> {
-            register(registeringsPane);
-            vindu.setScene(hovedScene.getScene());
+            try {
+                register(registeringsPane);
+                vindu.setScene(hovedScene.getScene());
+            } catch (IOException ex) {
+                System.out.println("Feil i registrering, prøv igjen\n"+ex);
+            }
         });
 
         //matchKriteriePane.finn.setOnAction(e -> vindu.setScene(hovedScene.getScene()));
@@ -37,21 +42,20 @@ public class Main extends Application {
         vindu.show();
     }
 
-    private void register(RegisteringsPane pane) {
+    private void register(RegisteringsPane pane) throws IOException {
         String navn = pane.txtNavn.getText();
-        String kjønn = (String)pane.kjønnToggleGroup.getSelectedToggle().getUserData();
+        String kjønn = "";
+        if(pane.kjønnToggleGroup.getSelectedToggle() != null)
+            kjønn = (String)pane.kjønnToggleGroup.getSelectedToggle().getUserData();
         String alder = pane.txtAlder.getText();
         String interesser = pane.txtInteresser.getText();
         String bosted = pane.txtBosted.getText();
         String tlf = pane.txtTlf.getText();
 
-        try {
-            klient.sendMessage("register!" + navn + "!" + kjønn + "!" + alder + "!" + interesser + "!" + bosted + "!" + tlf);
-            String id = klient.receiveMessage();
-            // Skriv denne iden til tekst filen
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
+        klient.sendMessage("register!" + navn + "!" + kjønn + "!" + alder + "!" + interesser + "!" + bosted + "!" + tlf);
+        String id = klient.receiveMessage();
+        // Skriv denne iden til tekst filen
+
     }
 
 
