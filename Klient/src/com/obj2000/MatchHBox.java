ebc60085fe94bb94bf2c647e81b0a056b46034e7
almeh <ala_mehersia@hotmail.com>
2020-05-07 01:26:34 +0200

    polish

--- Klient/src/com/obj2000/MatchHBox.java
+++ Klient/src/com/obj2000/MatchHBox.java
@@ -8,6 +8,8 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import static com.obj2000.Main.minId;
@@ -15,6 +17,7 @@ import static com.obj2000.Main.minId;
 public class MatchHBox extends HBox {
     Label lbKjonn = null;
     Label lbAlder = null;
+    Label lbBosted;
     String kjonn;
     String alder;
     String interesser;
@@ -23,10 +26,11 @@ public class MatchHBox extends HBox {
     Button visMatch;
     Node foreldreNode;
     ImageView profilBilde;
+    VBox infoCon;
 
     public MatchHBox(String id, String alder, String kjonn, String interesser, String bosted, Node foreldreNode){
 
-        super(20);
+        super(80);
         matchId = id;
         this.foreldreNode = foreldreNode;
         this.interesser = interesser;
@@ -34,14 +38,22 @@ public class MatchHBox extends HBox {
         this.kjonn = kjonn;
         this.alder = alder;
 
-        lbKjonn = new Label(kjonn);
-        lbAlder = new Label(alder);
+        infoCon = new VBox(3);
+        infoCon.setStyle("-fx-font-size: 15px;");
+        lbKjonn = new Label("Kjønn: " + kjonn);
+        lbAlder = new Label("Alder: " + alder);
+        lbBosted = new Label("Bosted: " + bosted);
+        infoCon.getChildren().addAll(lbKjonn, lbAlder, lbBosted);
+        infoCon.setAlignment(Pos.CENTER);
         setStyle("-fx-background-color: #E8E8E8;" + "-fx-border-style: solid inside;" + "-fx-font-size: 15px;");
         setAlignment(Pos.CENTER);
-        setPadding(new Insets(25, 12, 25, 150));
-        visMatch = new Button("Fa detaljer");
+        setPadding(new Insets(25, 2, 25, 10));
+        setHeight(70);
+        setMinWidth(348);
+        setAlignment(Pos.CENTER_RIGHT);
+        visMatch = new Button("Se mer");
         visMatch.setOnAction(event -> {visMatchInfo();});
-        getChildren().addAll(lbKjonn,lbAlder, visMatch);
+        getChildren().addAll(infoCon, visMatch);
     }
 
     /**
